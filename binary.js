fc8110b07a605cc4f50a6258bdcdb89c67b349b2
unknown <ganesh.srambikal@gmail.com>
2019-01-09 21:03:34 +0530

    done

--- binary.js
+++ binary.js
@@ -1,29 +1,29 @@
 
-// const list = [5, 4, 8, 3, 2, 7, 1]
+const list = [5, 4, 8, 3, 2, 7, 1]
 
-// const toFind = 4
-// const result = binarySearch(list, toFind)
+const toFind = 4
+const result = binarySearch(list, toFind)
 
-// console.log(result)
+console.log(result)
 
-// function binarySearch(arr, target) {
-//     arr.sort()
-//     let left = 0;
-//     let right = arr.length - 1;
-//     while (left <= right) {
-//         const mid = left + Math.floor((right - left) / 2);
-//         if (arr[mid] === target) {
-//             return console.log("found at " + mid + "th position");
-//         }
-//         if (arr[mid] < target) {
-//             left = mid + 1;
-//         } else {
-//             right = mid - 1;
-//         }
+function binarySearch(arr, target) {
+    arr.sort()
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2);
+        if (arr[mid] === target) {
+            return console.log("found at " + mid + "th position");
+        }
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
 
-//     }
-//     return console.log("Not found")
-// }
+    }
+    return console.log("Not found")
+}
 
 /**
  * Binary search is more efficient than linear search. 
